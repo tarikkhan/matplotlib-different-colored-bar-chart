1bbc7b8fe532f3ebb762be83013571ccd924ae4c
Tarik <tariksayshi@gmail.com>
2025-11-14 18:26:13 +0600

    scaled values between 0 and 1

--- plot.py
+++ plot.py
@@ -4,9 +4,14 @@ fig, ax = plt.subplots()
 
 fruits = ['apple', 'blueberry', 'banana', 'orange', 'olive']
 counts = [40, 100, 300, 55, 120]
+
+old_range = np.max(counts) - np.min(counts)
+new_range = 1 - 0.2
+new_counts = (((counts - np.min(counts)) * new_range) / old_range) + 0.2
+
 bar_colors = ['red', 'blue', 'yellow', 'orange', 'olive']
 
-ax.bar(fruits, counts, label=bar_colors, color=bar_colors)
+ax.bar(fruits, new_counts, label=bar_colors, color=bar_colors)
 
 ax.set_ylabel('Fruit Supply')
 ax.set_title('Fruit supply by kind and color')
