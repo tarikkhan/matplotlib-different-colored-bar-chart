4351d13808883b361a0aa4e2d15eae737f9017c4
Tarik <tariksayshi@gmail.com>
2025-11-14 19:05:26 +0600

    added documentation

--- helpers.py
+++ helpers.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 def scale_values(old_value, new_max=1.0, new_min=0.2):
+    """Scales old values to fit between a new range """
+    
     old_min = np.min(old_value)
     old_range = np.max(old_value) - np.min(old_value)
     new_range = new_max - new_min
